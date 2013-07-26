1ac42473acd048763b48b5fd9f468a42d07d43d2
sethvincent <sethvincent@gmail.com>
2013-07-26 11:19:02 -0700

    back to blog link

--- single.php
+++ single.php
@@ -5,7 +5,8 @@
     <div id="primary-content"> 
       <?php while (have_posts()) : the_post(); ?>
 
-        <div class="post <?php echo get_cat_item_classes(); ?> <?php echo get_tag_item_classes(); ?>">
+        <div class="post">
+          <p class="back-blog"><a href="/">Back to the blog</a></p>
           <h2 class="post-title"><?php the_title(); ?></h2>
           <div class="post-date"><?php the_time('F j, Y'); ?></div>
           <div class="post-body"><?php the_content(); ?></div>
