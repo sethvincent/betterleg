79583582d91e161243a142bc65f3ba8133d89b25
sethvincent <sethvincent@gmail.com>
2013-07-26 11:19:42 -0700

    typo fix

--- single.php
+++ single.php
@@ -6,7 +6,7 @@
       <?php while (have_posts()) : the_post(); ?>
 
         <div class="post">
-          <p class="back-blog"><a href="/">Back to the blog</a></p>
+          <p class="back-blog"><a href="/blog">Back to the blog</a></p>
           <h2 class="post-title"><?php the_title(); ?></h2>
           <div class="post-date"><?php the_time('F j, Y'); ?></div>
           <div class="post-body"><?php the_content(); ?></div>
