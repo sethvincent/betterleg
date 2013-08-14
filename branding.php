aa2f92b229f39f6806d8a16e393fc1f09b7d4bf5
sethvincent <sethvincent@gmail.com>
2013-08-14 15:56:12 -0700

    add top menu

--- branding.php
+++ branding.php
@@ -2,4 +2,10 @@
   <a href="<?php bloginfo('url'); ?>/">
     <img id="site-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.jpg"/>
   </a>
+  <ul id="main-nav">
+    <li><a href="/competence">competence</a></li>
+    <li><a href="independence">independence</a></li>
+    <li><a href="integrity">integrity</a></li>
+    <li><a href="research">research</a></li>
+  </ul>
 </div>
